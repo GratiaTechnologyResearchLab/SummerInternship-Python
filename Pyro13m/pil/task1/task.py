7ff188f02d55fda004ed33750199596020d82bc2
Pyro13m <mpriyom02@gmail.com>
2018-05-16 15:07:57 +0530

    JPG to Png

---
+++ Pyro13m/pil/task1/task.py
@@ -0,0 +1,34 @@
+import os
+from PIL import Image
+
+initialDirectoryName = 'pictures'
+
+allDirectories = os.listdir(initialDirectoryName)
+print(sallDirectories)
+for folders in allDirectories:
+    # Print foldername
+    print "Files inside folder: " + folders    # Go inside the corresponding folder
+    fullPath = os.path.join(initialDirectoryName, folders)    # Print all files inside the folder
+    allFilesinFolders = os.listdir(fullPath)
+    print(sallFilesinFolders)    # Copy to some other directory
+    
+    for files in allFilesinFolders:
+        # Source file
+        sourceFile = os.path.join(os.path.join(initialDirectoryName,folders), files)        ###########################
+        # Change 'png' to the name of the directory you want to create the output files
+        ###########################        # Check if directory exists or not
+       
+        if not os.path.exists(os.path.join(initialDirectoryName, folders + '/png')):
+            os.makedirs(os.path.join(initialDirectoryName, folders + '/png'))        # Destination file
+       
+        destFile = os.path.join(os.path.join(initialDirectoryName,folders + '/png'), files[:-4] + '.png')        # Convert file
+        os.system('convert ' + sourceFile + ' ' + destFile)
+		'''img = Image.open(sourceFile+''+destFile+'.png').convert('LA')
+		img.save(sourceFile+''+destFile+'.png')
+		'''
+		
+
+
+
+
+
